dd91a4c5a04564537980a09361bdb7065fe42698
BogachevaN <npdro@mail.ru>
2016-10-20 14:47:58 +0700

    Добавлено несколько функций

---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Rectangle.java
@@ -0,0 +1,10 @@
+package ru.stqa.pft.sandbox;
+
+/**
+ * Created by Natusik on 19.10.2016.
+ */
+public class Rectangle {
+
+  public double a;
+  public double b;
+}
---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Square.java
@@ -0,0 +1,9 @@
+package ru.stqa.pft.sandbox;
+
+/**
+ * Created by Natusik on 19.10.2016.
+ */
+public class Square {
+
+  public double l;
+}
