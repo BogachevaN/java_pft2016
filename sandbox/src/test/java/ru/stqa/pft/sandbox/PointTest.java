db2c25559726324343c60592822634744694fa33
BogachevaN <npdro@mail.ru>
2016-10-24 15:49:10 +0700

    Добавлены тесты для проверки вычисления расстояния между точками

---
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/PointTest.java
@@ -0,0 +1,28 @@
+package ru.stqa.pft.sandbox;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+/**
+ * Created by Natusik on 24.10.2016.
+ */
+public class PointTest {
+
+  @Test
+  public void testDistance1(){
+
+    Point p1 = new Point(5,3);
+    Point p2 = new Point(-10,-10);
+    Assert.assertEquals(p1.distance(p2),"19,85");
+
+  }
+
+  @Test
+  public void testDistance2(){
+
+    Point p3 = new Point(0,3);
+    Point p4 = new Point(10,10);
+    Assert.assertEquals(p3.distance(p4),"12,21");
+
+  }
+}
