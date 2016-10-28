469fbf4bd4dd495eefa7ec397ba7d978bec9015d
BogachevaN <npdro@mail.ru>
2016-10-28 21:41:40 +0700

    Создан тест для изменения группы

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/test/GroupModificationTests.java
@@ -0,0 +1,20 @@
+package ru.stqa.pft.addressbook.test;
+
+import org.testng.annotations.Test;
+import ru.stqa.pft.addressbook.model.GroupData;
+
+/**
+ * Created by Natusik on 28.10.2016.
+ */
+public class GroupModificationTests extends TestBase {
+
+  @Test
+  public void testGroupModification(){
+    app.getNavigationHelper().gotoGroupPage();
+    app.getGroupHalper().selectGroup();
+    app.getGroupHalper().initGroupModification();
+    app.getGroupHalper().fillGroupForm(new GroupData("test1", "test2", "test3"));
+    app.getGroupHalper().submitGroupModification();
+    app.getGroupHalper().returnToGroupPage();
+  }
+}
