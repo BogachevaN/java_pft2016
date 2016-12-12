41ae35c69d5e80e272dd835ea5126ec156f98ed7
BogachevaN <npdro@mail.ru>
2016-12-13 00:27:55 +0700

    Регистрация нового пользователя 2

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/FtpHelper.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/FtpHelper.java
@@ -18,7 +18,7 @@ public class FtpHelper  {
         ftp = new FTPClient();
     }
 
-    public void upload(File file, String backup, String target) throws IOException {
+    public void upload(File file, String target, String backup) throws IOException {
         ftp.connect(app.getProperty("ftp.host"));
         ftp.login(app.getProperty("ftp.login"), app.getProperty("ftp.password"));
         ftp.deleteFile(backup);
